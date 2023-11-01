28f2a5d3c3db66320eef69cd4f3b0ad86eef54fd
wiilsonk <134128672+wiilsonk@users.noreply.github.com>
2023-11-01 15:41:35 +0200

    Update CropYieldModel.py

--- CropYieldModel.py
+++ CropYieldModel.py
@@ -1,7 +1,7 @@
 class CropYieldModel:
 
     # all yours features to be used to predict
-    area : int = 1.995503
+    area : int = 1.398776
     crop : int = -1.298110
     rainfall : float = 0
     pesticides : int = 0
